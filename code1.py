f8e468ea086446aaf72ee14e70373509088eb5d8
Jane Doe <jane.doe@mail.com>
2019-11-19 10:04:36 +0100

    Create congratulatory scripts

---
+++ code1.py
@@ -0,0 +1,8 @@
+# This is pseudocode, congratulating a user on their progress
+import libraries
+
+progress = instruqt.user.course_level("Git")
+
+if progress=="intermediate":
+    print('''congratulations, you are already at intermediate level in Git.
+          Keep up the good work!''')
---
+++ code2.py
@@ -0,0 +1,8 @@
+# This is pseudocode, congratulating a user if it's their birthday
+import libraries
+
+birthday = instruqt.user.date_of_birth("Git")
+today = date.get(now)
+
+if today == birthday:
+    print("Happy Birthday")
